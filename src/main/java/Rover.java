9f168884ad07efe4c631394b7811d8ceba5b5e7a
Kumar2215 <kumar.kg279@gmail.com>
2025-01-21 23:20:35 +0800

    Add support for marking tasks

--- src/main/java/Rover.java
+++ src/main/java/Rover.java
@@ -18,7 +18,7 @@ public class Rover {
         System.out.println("What can I do for you?");
         System.out.println(divider);
 
-        String[] tasks = new String[100];
+        Task[] tasks = new Task[100];
         int taskCount = 0;
 
         while (true) {
@@ -28,13 +28,32 @@ public class Rover {
                 break;
             } else if (input.equalsIgnoreCase("list")) {
                 System.out.println(divider);
+                System.out.println("Here are the tasks in your list:");
                 for (int i = 0; i < taskCount; i++) {
                     System.out.println((i + 1) + ". " + tasks[i]);
                 }
                 System.out.println(divider);
                 continue;
+            } else if (input.toLowerCase().startsWith("mark")) {
+                String[] parts = input.split(" ");
+                int index = Integer.parseInt(parts[1]) - 1;
+                tasks[index].setDone();
+                System.out.println(divider);
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println(tasks[index]);
+                System.out.println(divider);
+                continue;
+            } else if (input.toLowerCase().startsWith("unmark")) {
+                String[] parts = input.split(" ");
+                int index = Integer.parseInt(parts[1]) - 1;
+                tasks[index].setUndone();
+                System.out.println(divider);
+                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.println(tasks[index]);
+                System.out.println(divider);
+                continue;
             }
-            tasks[taskCount] = input;
+            tasks[taskCount] = new Task(input);
             taskCount++;
             System.out.println(divider);
             System.out.println("added: " + input);
