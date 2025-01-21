04f9403932b7eb08dd003144069748e7d024a430
Kumar2215 <kumar.kg279@gmail.com>
2025-01-21 22:38:41 +0800

    Add greeting

--- src/main/java/Rover.java
+++ src/main/java/Rover.java
@@ -1,10 +1,21 @@
 public class Rover {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+
+        String divider = "--------------------------------------------";
+
+        String logo = "██████╗  ██████╗ ██╗   ██╗███████╗██████╗ \n" +
+                "██╔══██╗██╔═══██╗██║   ██║██╔════╝██╔══██╗\n" +
+                "██████╔╝██║   ██║██║   ██║█████╗  ██████╔╝\n" +
+                "██╔══██╗██║   ██║╚██╗ ██╔╝██╔══╝  ██╔══██╗\n" +
+                "██║  ██║╚██████╔╝ ╚████╔╝ ███████╗██║  ██║\n" +
+                "╚═╝  ╚═╝ ╚═════╝   ╚═══╝  ╚══════╝╚═╝  ╚═╝\n";
+
+        System.out.println(divider);
+        System.out.println("Hello! I'm Rover");
+        System.out.println(logo);
+        System.out.println("What can I do for you?");
+        System.out.println(divider);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(divider);
     }
 }
